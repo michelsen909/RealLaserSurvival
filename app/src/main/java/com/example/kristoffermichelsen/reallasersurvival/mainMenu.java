4f7c17609cce827aed4d285e3e6af5988a843e9f
Kristoffer Michelsen <michelsen909@hotmail.se>
2016-06-14 09:49:12 +0200

    Initial

---
+++ app/src/main/java/com/example/kristoffermichelsen/reallasersurvival/mainMenu.java
@@ -0,0 +1,29 @@
+package com.example.kristoffermichelsen.reallasersurvival;
+
+import android.content.Intent;
+import android.media.audiofx.BassBoost;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class mainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+
+        Button startGame = (Button) findViewById(R.id.button);
+
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent start = new Intent(mainMenu.this, SettingsActivity.class);
+                startActivity(start);
+            }
+        });
+
+     }
+}
