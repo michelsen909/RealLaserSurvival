3dcc9423c0b4411d37a29d8729e907046d29f6f8
MBJohansen <mads94_johansen@hotmail.com>
2016-06-15 10:57:38 +0200

    Added HighscoreActivity

---
+++ app/src/main/java/com/example/kristoffermichelsen/reallasersurvival/HighscoreActivity.java
@@ -0,0 +1,26 @@
+package com.example.kristoffermichelsen.reallasersurvival;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighscoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscore);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
