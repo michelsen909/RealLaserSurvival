f6eefb44b8fad408d93a88c26398cd38225165d7
Skulldj <skulldj2@gmail.com>
2016-06-15 10:25:05 +0200

    Ordnet overskrift i game over

--- app/src/main/java/com/example/kristoffermichelsen/reallasersurvival/GameOverActivity.java
+++ app/src/main/java/com/example/kristoffermichelsen/reallasersurvival/GameOverActivity.java
@@ -1,10 +1,12 @@
 package com.example.kristoffermichelsen.reallasersurvival;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class GameOverActivity extends AppCompatActivity {
 
@@ -12,6 +14,11 @@ public class GameOverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        TextView title = (TextView) findViewById(R.id.title);
+        Typeface font = Typeface.createFromAsset(getAssets(),"fonts/ARDESTINE.ttf");
+        title.setTypeface(font);
+        title.setTextSize(45);
 
         Button playAgain = (Button) findViewById(R.id.playAgainButton);
 
