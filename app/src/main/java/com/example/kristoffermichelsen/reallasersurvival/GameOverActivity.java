ba9fadef060e432f7a73664f843cfdc4da0a987e
Skulldj <skulldj2@gmail.com>
2016-06-15 10:23:05 +0200

    Ordnet knapper i gameover

--- app/src/main/java/com/example/kristoffermichelsen/reallasersurvival/GameOverActivity.java
+++ app/src/main/java/com/example/kristoffermichelsen/reallasersurvival/GameOverActivity.java
@@ -1,7 +1,10 @@
 package com.example.kristoffermichelsen.reallasersurvival;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameOverActivity extends AppCompatActivity {
 
@@ -10,6 +13,25 @@ public class GameOverActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
 
+        Button playAgain = (Button) findViewById(R.id.playAgainButton);
+
+        playAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent playAgain = new Intent(GameOverActivity.this, GameActivity.class);
+                startActivity(playAgain);
+            }
+        });
+
+        Button mainMenu = (Button) findViewById(R.id.mainMenuButton);
+
+        mainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainMenu = new Intent(GameOverActivity.this, MainMenuActivity.class);
+                startActivity(mainMenu);
+            }
+        });
 
 
 
