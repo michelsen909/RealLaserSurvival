22d2742c3507d2e33c3f310d4b7768ae8094dc68
MBJohansen <mads94_johansen@hotmail.com>
2016-06-15 09:31:28 +0200

    Some push error after GameActivity and GameOverActivity push

--- app/src/main/java/com/example/kristoffermichelsen/reallasersurvival/GameOverActivity.java
+++ app/src/main/java/com/example/kristoffermichelsen/reallasersurvival/GameOverActivity.java
@@ -9,5 +9,10 @@ public class GameOverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+
+
+
+
     }
 }
