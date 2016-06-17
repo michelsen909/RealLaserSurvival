748bdd982d61762e75f74b6f07d89e08226f13a5
Mehmet <Chris_12359@hotmail.com>
2016-06-17 10:31:04 +0200

    Done with Highscore, for now. Added HighscoreEntry Class, pre Gson+Json

--- app/src/main/java/com/example/kristoffermichelsen/reallasersurvival/HighscoreActivity.java
+++ app/src/main/java/com/example/kristoffermichelsen/reallasersurvival/HighscoreActivity.java
@@ -24,6 +24,18 @@ public class HighscoreActivity extends AppCompatActivity {
         title.setTypeface(font);
         title.setTextSize(45);
 
+        TextView no1 = (TextView) findViewById(R.id.no1);
+        TextView no2 = (TextView) findViewById(R.id.no2);
+        TextView no3 = (TextView) findViewById(R.id.no3);
+        TextView no4 = (TextView) findViewById(R.id.no4);
+        TextView no5 = (TextView) findViewById(R.id.no5);
+
+        no1.setText("#1 "+MainMenuActivity.highscores.get(0));
+        no2.setText("#2 "+MainMenuActivity.highscores.get(1));
+        no3.setText("#3 "+MainMenuActivity.highscores.get(2));
+        no4.setText("#4 "+MainMenuActivity.highscores.get(3));
+        no5.setText("#5 "+MainMenuActivity.highscores.get(4));
+
         Button mainMenu = (Button) findViewById(R.id.mainMenuButton);
 
         mainMenu.setOnClickListener(new View.OnClickListener() {
