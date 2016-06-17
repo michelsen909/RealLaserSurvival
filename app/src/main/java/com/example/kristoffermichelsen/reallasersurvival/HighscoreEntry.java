0300200337e049fe01976088a78573d6f7831d70
Mehmet <Chris_12359@hotmail.com>
2016-06-17 13:47:56 +0200

    Nothing of worth

--- app/src/main/java/com/example/kristoffermichelsen/reallasersurvival/HighscoreEntry.java
+++
@@ -1,9 +0,0 @@
-package com.example.kristoffermichelsen.reallasersurvival;
-
-/**
- * Created by Christian on 17-06-2016.
- */
-public class HighscoreEntry {
-    static int highscore;
-    static char user;
-}
